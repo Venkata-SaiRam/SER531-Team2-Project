2f3a7628bf6f4f302328b0dc9d32461c70a342c7
Aravind <akondama@asu.edu>
2019-12-06 14:45:36 -0700

    Complete End to end flow commited.

--- src/main/java/com/SER531/crimeRateAnalysis/SemanticQuery.java
+++ src/main/java/com/SER531/crimeRateAnalysis/SemanticQuery.java
@@ -1,7 +1,6 @@
 package com.SER531.crimeRateAnalysis;
 
 import com.SER531.crimeRateAnalysis.beans.*;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSet;
@@ -43,6 +42,11 @@ public class SemanticQuery {
         ViolentCrimeRates violentCrimeRates = new ViolentCrimeRates();
         if (mock != null && mock) {
             json = "{\"head\":{\"vars\":[\"Murder\",\"robbery\",\"Rape2\",\"Population\",\"Aggravatedassault\",\"Rape1\",\"ViolentCrime\"]},\"results\":{\"bindings\":[{\"Aggravatedassault\":{\"type\":\"literal\",\"value\":\"506\"},\"Population\":{\"type\":\"literal\",\"value\":\"178654\"},\"ViolentCrime\":{\"type\":\"literal\",\"value\":\"902\"},\"Murder\":{\"type\":\"literal\",\"value\":\"4\"},\"robbery\":{\"type\":\"literal\",\"value\":\"237\"},\"Rape1\":{\"type\":\"literal\",\"value\":\"155\"},\"Rape2\":{\"type\":\"literal\",\"value\":\"0\"}}]}}\n";
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } else {
             ResultSet results = this.execSelectAndPrint("http://34.66.37.163:3030/ViolentCrime/query", "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
@@ -107,6 +111,7 @@ public class SemanticQuery {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Finished Violent Crimes.");
         return violentCrimeRates;
     }
 
@@ -115,6 +120,11 @@ public class SemanticQuery {
         String json = null;
         if (mock != null && mock) {
             json = "{\"head\":{\"vars\":[\"PropertyCrimevalue\",\"MotorVehicleCrimevalue\",\"BurglaryCrimevalue\",\"ArsonCrimevalue\",\"LarcenyCrimevalue\"]},\"results\":{\"bindings\":[{\"PropertyCrimevalue\":{\"type\":\"literal\",\"value\":\"8144\"},\"BurglaryCrimevalue\":{\"type\":\"literal\",\"value\":\"1200\"},\"MotorVehicleCrimevalue\":{\"type\":\"literal\",\"value\":\"450\"},\"ArsonCrimevalue\":{\"type\":\"literal\",\"value\":\"38\"},\"LarcenyCrimevalue\":{\"type\":\"literal\",\"value\":\"6494\"}}]}}";
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } else {
             ResultSet results = this.execSelectAndPrint("http://104.197.185.80:3030/PropertyCrime/query",
                     "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" +
@@ -172,6 +182,7 @@ public class SemanticQuery {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Finished Property.");
         return propertyCrimeRates;
     }
 
@@ -185,6 +196,11 @@ public class SemanticQuery {
         String json = null;
         if(mock != null && mock){
             json = "{\"head\":{\"vars\":[\"States\"]},\"results\":{\"bindings\":[{\"States\":{\"type\":\"literal\",\"value\":\"ALABAMA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"ALASKA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"ARIZONA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"ARKANSAS\"}},{\"States\":{\"type\":\"literal\",\"value\":\"CALIFORNIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"COLORADO\"}},{\"States\":{\"type\":\"literal\",\"value\":\"CONNECTICUT\"}},{\"States\":{\"type\":\"literal\",\"value\":\"DELAWARE\"}},{\"States\":{\"type\":\"literal\",\"value\":\"DISTRICT OF COLUMBIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"FLORIDA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"GEORGIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"HAWAII\"}},{\"States\":{\"type\":\"literal\",\"value\":\"IDAHO\"}},{\"States\":{\"type\":\"literal\",\"value\":\"ILLINOIS\"}},{\"States\":{\"type\":\"literal\",\"value\":\"INDIANA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"IOWA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"KANSAS\"}},{\"States\":{\"type\":\"literal\",\"value\":\"KENTUCKY\"}},{\"States\":{\"type\":\"literal\",\"value\":\"LOUISIANA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MAINE\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MARYLAND\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MASSACHUSETTS\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MICHIGAN\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MINNESOTA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MISSISSIPPI\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MISSOURI\"}},{\"States\":{\"type\":\"literal\",\"value\":\"MONTANA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEBRASKA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEVADA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEW HAMPSHIRE\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEW JERSEY\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEW MEXICO\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NEW YORK\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NORTH CAROLINA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"NORTH DAKOTA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"OHIO\"}},{\"States\":{\"type\":\"literal\",\"value\":\"OKLAHOMA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"OREGON\"}},{\"States\":{\"type\":\"literal\",\"value\":\"PENNSYLVANIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"RHODE ISLAND\"}},{\"States\":{\"type\":\"literal\",\"value\":\"SOUTH CAROLINA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"SOUTH DAKOTA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"TENNESSEE\"}},{\"States\":{\"type\":\"literal\",\"value\":\"TEXAS\"}},{\"States\":{\"type\":\"literal\",\"value\":\"UTAH\"}},{\"States\":{\"type\":\"literal\",\"value\":\"VERMONT\"}},{\"States\":{\"type\":\"literal\",\"value\":\"VIRGINIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"WASHINGTON\"}},{\"States\":{\"type\":\"literal\",\"value\":\"WEST VIRGINIA\"}},{\"States\":{\"type\":\"literal\",\"value\":\"WISCONSIN\"}},{\"States\":{\"type\":\"literal\",\"value\":\"WYOMING\"}}]}}";
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }else {
             ResultSet results = this.execSelectAndPrint("http://35.193.121.187:3030/GeographicInformation/query", "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
@@ -225,6 +241,11 @@ public class SemanticQuery {
         String json = null;
         if(mock != null && mock){
             json = "{\"head\":{\"vars\":[\"Cities\"]},\"results\":{\"bindings\":[{\"Cities\":{\"type\":\"literal\",\"value\":\"Apache Junction\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Avondale\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Buckeye\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Bullhead City\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Camp Verde\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Casa Grande\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Chandler\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Chino Valley\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Clarkdale\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Coolidge\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Cottonwood\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Douglas\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Eagar\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"El Mirage\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Eloy\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Flagstaff\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Florence\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Fredonia\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Gilbert\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Glendale\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Goodyear\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Holbrook\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Jerome\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Kearny\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Kingman\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Lake Havasu City\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Marana\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Maricopa\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Mesa\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Miami\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Nogales\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Oro Valley\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Page\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Paradise Valley\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Patagonia\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Payson\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Peoria\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Phoenix\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Pima\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Pinetop-Lakeside\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Prescott\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Prescott Valley\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Safford\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Sahuarita\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"San Luis\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Scottsdale\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Show Low\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Sierra Vista\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Snowflake-Taylor\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Somerton\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Springerville\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Surprise\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Tempe\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Thatcher\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Tucson\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Wellton\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Wickenburg\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Willcox\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Williams\"}},{\"Cities\":{\"type\":\"literal\",\"value\":\"Winslow\"}}]}}";
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }else {
             boolean flag = (state == null) || (state == "") || state == " ";
             state = flag ? "ALABAMA" : state;
